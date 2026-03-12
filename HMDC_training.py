934b501fa59e29ac019263e9165b9a608478989c
THDohb <84966033+telagment@users.noreply.github.com>
2026-03-12 10:56:07 +0100

    Modify training parameters and add pruning score
    Updated argument defaults and added pruning score option for model training.

--- HMDC_training.py
+++ HMDC_training.py
@@ -39,10 +39,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Training HMDC models on tabular datasets')
     parser.add_argument('-dataset', choices=DATASETS, default='Adult', type=str, help='Dataset')
     parser.add_argument('--baselearner', choices=BASELEARNERS, default='lr', type=str, help='Base Learner')
-    parser.add_argument('--palim', type=int, default=0, help='The maximum number of parents for each node')
-    parser.add_argument('--plot', action=argparse.BooleanOptionalAction, help='Whether or not to plot the BN structure')
-    parser.add_argument('--is-missing', action=argparse.BooleanOptionalAction, default=True, help='Whether or not missing data appear in inference')
-    parser.add_argument('--n-folds', type=int, default=10, help='Number of folds')
+    parser.add_argument('--palim', type=int, default=2, help='The maximum number of parents for each node')
+    parser.add_argument('--n-folds', type=int, default=5, help='Number of folds')
+    parser.add_argument('--pruning-score', type=str, choices=['BIC', 'AIC'], default='BIC', help='Pruning score for learning')
     parser.add_argument('--grouping', action=argparse.BooleanOptionalAction, default=False, help='Whether or not to group discrete features')
     parser.add_argument('--output', type=str, default='experiments_tabular/HMDC_training', help='Output path')
     args = parser.parse_args()
@@ -50,18 +49,11 @@ if __name__ == '__main__':
     dataset = args.dataset
     base = args.baselearner
     palim = args.palim
-    disclim = args.disclim
-    is_plot = args.plot
     n_folds = args.n_folds
     output_path = args.output
+    pruning_score = args.pruning_score
     grouping = args.grouping
-    base_dir = os.path.join(output_path, base)
-    os.makedirs(base_dir, exist_ok=True)
-    dataset_dir = os.path.join(base_dir, dataset)
-    os.makedirs(dataset_dir, exist_ok=True)
-
-
-    model_dir = dataset_dir
+    model_dir = os.path.join(output_path, pruning_score, base, dataset)
     os.makedirs(model_dir, exist_ok=True)
 
     if not(model_dir is None):
@@ -71,32 +63,25 @@ if __name__ == '__main__':
     else:
         raise Exception('Can not find Result path!')
     
-    logging.info('{}{}{}{}{}{}{}{}'.format('Base learner: ', base, 'Dataset:', dataset))
+    logging.info('{}{}{}{}'.format('Base learner: ', base, 'Dataset:', dataset))
     logging.info('{}{}{}{}'.format('No folds : ', n_folds, 'Palim : ', palim))
+    logging.info('{}{}{}{}'.format('Pruning score : ', pruning_score, 'Grouping : ', grouping))
 
     mat_path = os.path.join(DATASETS_DIR, dataset, dataset) + '.mat'
     data_loader = TabularDataLoader_HMDC(mat_path=mat_path, dataset=dataset)
     kf = KFold(n_splits=n_folds, shuffle=True, random_state=42)
-
-    h_hls_opt, h_zos_opt, s_hls_opt, s_zos_opt = [], [], [], []
-    h_hls_ave, h_zos_ave, s_hls_ave, s_zos_ave = [], [], [], []
-    h_hls_ref, h_zos_ref, s_hls_ref, s_zos_ref = [], [], [], []
-    imp_h_hls, imp_s_hls, imp_h_zos, imp_s_zos = [], [], [], []
-    imp_h_hls_02, imp_s_hls_02, imp_h_zos_02, imp_s_zos_02 = [], [], [], []
-    ran_zeros, ran_hams = [], []
-    imp_zeros, imp_hams = [], []
-    br_hls, br_zos = [], []
-    ran_deltas, imp_deltas, br_deltas = [], [], []
-    opt_deltas, ave_deltas, ref_deltas = [], [], []
-    train_splits = []
-    test_splits = []
     
     # Merge random pairs of discrete features to reduce the number of discrete features
     if dataset == 'Thyroid': 
         disc_domains, disc_features, disc_names = merge_random_triple(data_loader.discrete_feature_domains, 
                                                                     data_loader.discrete_features, 
                                                                     data_loader.discrete_feature_names) 
-            
+        logging.info(f"Thyroid discrete features group {(disc_names)}")
+        # saving disc-names
+        with open(os.path.join(model_dir, "disc_names.txt"), 'w') as f:
+            for item in disc_names:
+                f.write("%s\n" % item)
+
         # 2. Map the new node names to the index-based names (using their index in disc_names)
         no_y = len(data_loader.label_names)
         indexed_disc_names = [str(i+no_y) for i in range(len(disc_names))]
@@ -128,9 +113,9 @@ if __name__ == '__main__':
         # GBNC
         time_time = time.time()
         hmdc = TabularClassifier_HMDC(palim=palim, base=base, result_path=saving_fold)
-        hmdc.fit(train_data_loader, save_baselearner=True)
+        hmdc.fit(train_data_loader, pruning_score=pruning_score, save_baselearner=True)
 
-        hmdc.learn_structure()
+        hmdc.learn_structure(pruning_score=pruning_score)
         logging.info(f"Time to fit and learn structure: {time.time() - time_time}") 
         time_time = time.time()
 
