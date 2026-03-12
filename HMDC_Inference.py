325ef620b3f5a274926d39945f0aea80c683cc07
THDohb <84966033+telagment@users.noreply.github.com>
2026-03-12 10:57:46 +0100

    Update HMDC_Inference.py

--- HMDC_Inference.py
+++ HMDC_Inference.py
@@ -33,7 +33,7 @@ BASELEARNERS = [
     'svm',
 ]
 
-DATASETS_DIR = '/MDC_data'
+DATASETS_DIR = 'MDC_data'
 
 
 if __name__ == '__main__':
@@ -45,61 +45,53 @@ if __name__ == '__main__':
     # parser.add_argument('--dis-missing', type=float, default=0.8, help='The percentage of missing discrete features ')
     # parser.add_argument('--class-missing', type=float, default=0.8, help='The percentage of missing class variables')
     parser.add_argument('--is-missing', action=argparse.BooleanOptionalAction, default=True, help='Whether or not missing data appear in inference')
-    parser.add_argument('--n-folds', type=int, default=10, help='Number of folds')
-    parser.add_argument('--output', type=str, default='experiments_tabular/HMDC_inference_rest', help='Output path')
+    parser.add_argument('--n-folds', type=int, default=5, help='Number of folds')
+    parser.add_argument('--pruning-score', type=str, choices=['BIC', 'AIC'], default='BIC', help='Pruning score for learning')
+    parser.add_argument('--grouping', action=argparse.BooleanOptionalAction, default=False, help='Whether or not to group Adult discrete features')
+    parser.add_argument('--load_dir', type=str, default='experiments_tabular/HMDC_training/BIC/lr/Adult/data', help='Output path')
+    parser.add_argument('--output', type=str, default='experiments_tabular/HMDC_inference', help='Output path')
     args = parser.parse_args()
 
     dataset = args.dataset
     base = args.baselearner
     palim = args.palim
-    disclim = args.disclim
     n_folds = args.n_folds
     output_path = args.output
     is_missing = args.is_missing
+    load_dir = args.load_dir 
+    grouping = args.grouping
+    pruning_score = args.pruning_score
+    
+    dis_missing_list = [0.3, 0.8, 1.0]
+    class_missing_list = [0.3, 0.7, 0.8, 0.9, 1.0]
 
-    dis_missing_list = [0.3, 0.8]
-    class_missing_list = [0.3, 0.7, 0.8, 0.9]
-
-    base_dir = os.path.join(output_path, base)
-    os.makedirs(base_dir, exist_ok=True)
-    dataset_dir = os.path.join(base_dir, dataset)
+    dataset_dir = os.path.join(output_path, pruning_score, base, dataset)
     os.makedirs(dataset_dir, exist_ok=True)
 
-    data_loader_dir = 'experiments_tabular/HMDC_training/' + str(base) + '/' + str(dataset)
+    # data_loader_dir = 'experiments_tabular/HMDC_training/' + str(base) + '/' + str(dataset)
     mat_path = os.path.join(DATASETS_DIR, dataset, dataset) + '.mat'
     if dataset == 'Thyroid': 
-        with open(os.path.join(data_loader_dir, "data_loader.pkl"), 'rb') as f:
+        with open(os.path.join(load_dir, "data_loader.pkl"), 'rb') as f:
             data_loader = pickle.load(f)
     else:
         data_loader = TabularDataLoader_HMDC(mat_path=mat_path, dataset=dataset)
+    
+    if dataset == 'Adult' and grouping == True: 
+        data_loader.update(data_loader, node_id=3) 
 
-    load_dir = 'experiments_tabular/HMDC_training/' + str(base) + '/' + str(dataset) + '/data'
+    # load_dir = 'experiments_tabular/HMDC_training/' + str(base) + '/' + str(dataset) + '/data'
     for dis_missing in dis_missing_list:
         for class_missing in class_missing_list:
-            h_hls_opt, h_zos_opt, s_hls_opt, s_zos_opt = [], [], [], []
-            h_hls_ave, h_zos_ave, s_hls_ave, s_zos_ave = [], [], [], []
-            h_hls_ref, h_zos_ref, s_hls_ref, s_zos_ref = [], [], [], []
-            imp_h_hls, imp_s_hls, imp_h_zos, imp_s_zos = [], [], [], []
-            imp_h_hls_02, imp_s_hls_02, imp_h_zos_02, imp_s_zos_02 = [], [], [], []
-            ran_zeros, ran_hams = [], []
-            imp_zeros, imp_hams = [], []
-            br_hls, br_zos = [], []
-            ran_deltas, imp_deltas, br_deltas = [], [], []
-            opt_deltas, ave_deltas, ref_deltas = [], [], []
-            train_splits = []
-            test_splits = []
             model_dir = os.path.join(dataset_dir, str(class_missing) + '_' + str(dis_missing))
             os.makedirs(model_dir, exist_ok=True)
-
             saving_dir = os.path.join(model_dir, 'test_data')
             os.makedirs(saving_dir, exist_ok=True)
-            
             log_file = os.path.join(model_dir, 'log_file.log')
-            logging.basicConfig(level=logging.INFO, filename=log_file, filemode="w")
 
-            logging.info(f'python mixed_data_missing_inference_HMDC_Imb_Inference_loop.py -dataset {dataset} --baselearner {base} \
-                    --palim {palim} --disclim {disclim} --dis-missing {dis_missing} --class-missing {class_missing} \
-                    --n-folds {n_folds} --output {output_path} --is-missing {is_missing}')
+            logging.basicConfig(level=logging.INFO, filename=log_file, filemode="w")
+            logging.info(f'python HMDC_Inference.py -dataset {dataset} --baselearner {base} \
+                    --palim {palim} --dis-missing {dis_missing} --class-missing {class_missing} \
+                    --n-folds {n_folds} --output {output_path} --is-missing {is_missing} --pruning_score {pruning_score}')
             
             for i in range(n_folds):
                 saving_fold = os.path.join(saving_dir, str(i))
@@ -121,7 +113,7 @@ if __name__ == '__main__':
                 random_clf.fit(test_data_loader.continuous_features, test_data_loader.all_labels)
                 y_pred_ran = random_clf.predict(test_data_loader.continuous_features)
 
-                # # Imputation - Mode Imputeation
+                # # Imputation - Mode Imputation
                 most_frequent_classes = [stats.mode(train_data_loader.labels[:, col])[0][0] for col in range(train_data_loader.labels.shape[1])]
                 y_pred_imp = test_data_loader.labels.copy()
                 for col in range(test_data_loader.labels.shape[1]):
@@ -135,35 +127,37 @@ if __name__ == '__main__':
                 br_pred = br.predict(X=test_data_loader.continuous_features)
                 br_pred = br_pred.toarray()
 
-                # GBNC
+                # hmdc
                 time_time = time.time()
-                gbnc = TabularClassifier_HMDC(palim=palim, base=base, result_path=model_dir)
+                hmdc = TabularClassifier_HMDC(palim=palim, base=base, result_path=model_dir)
                 score_path = os.path.join(load_fold, "score")
                 classifier_path = os.path.join(load_fold, "classifiers.pkl")
                 # Load the learned structure and parameters
-                gbnc.load_DAG(score_path, classifier_path, test_data_loader)
+                hmdc.load_DAG(score_path, classifier_path, test_data_loader)
 
-                gbnc.learn_structure()
+                hmdc.learn_structure(pruning_score=pruning_score)
 
-                gbnc_s_imp, gbnc_h_imp = gbnc.inference_missing_imp(train_data_loader=train_data_loader, test_data_loader=test_data_loader)
+                hmdc_s_imp, hmdc_h_imp = hmdc.inference_missing_imp(train_data_loader=train_data_loader, test_data_loader=test_data_loader)
 
-                gbnc_s_pred_opt, gbnc_h_pred_opt = gbnc.inference_missing_opt(test_data_loader) 
+                hmdc_s_pred_opt, hmdc_h_pred_opt = hmdc.inference_missing_opt(test_data_loader) 
 
-                gbnc_s_pred_ave, gbnc_h_pred_ave = gbnc.inference_missing_ave(test_data_loader)
+                hmdc_s_pred_ave, hmdc_h_pred_ave = hmdc.inference_missing_ave(test_data_loader)
 
-                gbnc_s_pred_ref, gbnc_h_pred_ref = gbnc.inference_missing_ref(test_data_loader=test_data_loader)
+                hmdc_s_pred_ref, hmdc_h_pred_ref = hmdc.inference_missing_ref(test_data_loader=test_data_loader)
 
                 np.savez(os.path.join(saving_fold, "predictions.npz"),
+                            y_true=test_data_loader.labels,
+                            y_all_labels=test_data_loader.all_labels,
                             y_pred_ran=y_pred_ran,
                             y_pred_imp=y_pred_imp,
                             br_pred=br_pred,
-                            gbnc_h_imp=gbnc_h_imp,
-                            gbnc_s_imp=gbnc_s_imp,
-                            gbnc_h_pred_opt=gbnc_h_pred_opt,
-                            gbnc_s_pred_opt=gbnc_s_pred_opt,
-                            gbnc_h_pred_ave=gbnc_h_pred_ave,
-                            gbnc_s_pred_ave=gbnc_s_pred_ave,
-                            gbnc_s_pred_ref=gbnc_s_pred_ref,
-                            gbnc_h_pred_ref=gbnc_h_pred_ref,
+                            hmdc_h_imp=hmdc_h_imp,
+                            hmdc_s_imp=hmdc_s_imp,
+                            hmdc_h_pred_opt=hmdc_h_pred_opt,
+                            hmdc_s_pred_opt=hmdc_s_pred_opt,
+                            hmdc_h_pred_ave=hmdc_h_pred_ave,
+                            hmdc_s_pred_ave=hmdc_s_pred_ave,
+                            hmdc_s_pred_ref=hmdc_s_pred_ref,
+                            hmdc_h_pred_ref=hmdc_h_pred_ref,
                         )
     
